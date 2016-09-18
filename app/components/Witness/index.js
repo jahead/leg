8170e111b9f4890a7202a63ba9ce20ad4d9aa98d
Joshua Head <joshua.head@outlook.com>
2016-09-18 19:01:00 +1000

    added missing witness

---
+++ app/components/Witness/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.css';
+
+const Witness = () => {
+    return (
+        <div className={styles['wrap-flexbox']}>
+            <img src="images/Witness/Witness.png" className={'png ' + styles['inner-flexbox']+ ' ' + styles.witness}>
+            </img>
+        </div>
+    );
+}
+
+export default Witness;
